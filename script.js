229735b800ae6b0a830770bd66c32d4e776900fc
3H-Gastao Gabriel Felipe Fernandes Ribeiro <108344738+gabriel24felipe@users.noreply.github.com>
2024-03-11 11:11:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ function mensagem(){
 
 imprime = (msg) => console.log(msg);
 
+function sorteia(){
+    let num1 = document.getElementById("inicio").value;
+    let num2 = document.getElementById("fim").value;
+    num2 = Number(num2)+1;
+    let sorteio = Math.floor(Math.random(inicio, fim+1)* (Number(num2) - Number(num1)));
 
+    document.getElementById("resultado").innerHTML = sorteio;
+}
