f29c8a7aee5787739b526162b4f2b2ae5df6bb78
3H-Gastao Gabriel Felipe Fernandes Ribeiro <108344738+gabriel24felipe@users.noreply.github.com>
2024-03-18 11:22:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-function mensagem(){
-    alert("cu");
-}
-
 imprime = (msg) => console.log(msg);
 
-function sorteia(){
-    let num1 = document.getElementById("inicio").value;
-    let num2 = document.getElementById("fim").value;
-    num2 = Number(num2)+1;
-    let sorteio = Math.floor(Math.random(inicio, fim+1)* (Number(num2) - Number(num1)));
+FazerSorteio = () => {
+     let num1 = document.getElementById("inicio").value;
+     let num2 = document.getElementById("fim").value;
+     let sorteio = Math.floor( Math.random(inicio,fim+1)*100);
 
-    document.getElementById("resultado").innerHTML = sorteio;
+     document.getElementById("resultado").innerHTML = sorteio;
+
+}
+
+function escolher_aba(btn){
+     for(let i = 1; i <= 4; i++) {
+          let id_btn = "btn"+i;
+          let id_aba = "aba"+i;
+          document.getElementById(id_btn).style.backgroundColor = "blue";
+          document.getElementById(id_btn).disabled = false;
+          document.getElementsById(id_aba).style.display = "none"
+     }
+     if(btn == "1"){
+     document.getElementById("btn1").style.backgroundColor = "red";
+     document.getElementById("btn1").disabled = true;
+     document.getElementById("ab1")
+    }
+     if(btn == "2"){
+     document.getElementById("btn2").style.backgroundColor = "red";
+     document.getElementById("btn2").disabled = true;
+}
+     if(btn == "3"){
+     document.getElementById("btn3").style.backgroundColor = "red";
+     document.getElementById("btn3").disabled = true;
+}
+     if(btn == "4"){
+     document.getElementById("btn4").style.backgroundColor = "red";
+     document.getElementById("btn4").disabled = true;
+}
 }
