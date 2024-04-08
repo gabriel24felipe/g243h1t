592cceb45ec46694cf1f4beaf5e5ae92b9747722
3H-Gastao Gabriel Felipe Fernandes Ribeiro <108344738+gabriel24felipe@users.noreply.github.com>
2024-04-08 11:14:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,68 @@
 imprime = (msg) => console.log(msg);
-const data = new Date();
-function hora_atual(){
-    setTimeout(() => {
-    document.getElementById("h").innerHTML = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
-    hora_atual();
-    },1000);
-}
-function sorteio(){
-    let num1 = document.getElementById("inicio").value;
-    let num2 = document.getElementById("fim").value;
-    num2 = Number(num2)+1;
-    let sorteio = Math.floor(Math.random(inicio, fim+1)* (Number(num2) - Number(num1)));
 
+fazerSorteio = () => {
+    num1 = document.getElementById("inicio").value;
+    num2 = document.getElementById("fim").value;
+    let sorteio = Math.floor(Math.random() * (Number(num2) - Number(num1)) );
     document.getElementById("resultado").innerHTML = sorteio;
+
+}
+imprime = (msg) => console.log(msg);
+
+function hora_atual (){
+     const data = new Date();
+     
+    setTimeout(() => {
+        document.getElementById("h").innerHTML = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+        hora_atual();
+    }, 1000);
 }
 
-function escolher_aba(btn){   
+function escolher_aba(btn){
     for(let i = 1; i <= 4; i++){
-    let id_btn = "btn"+i;
-    let id_aba = "ab"+i;
-    
-document.getElementById(id_btn).style.background = "blue";
-document.getElementById(id_btn).disabled = false;
-document.getElementById(id_aba).style.display = "none";
+        let id_btn = "btn"+i;
+        let id_aba = "ab"+i;
+        document.getElementById(id_btn).style.backgroundColor = "blueviolet";
+        document.getElementById(id_btn).disabled = false;
+        document.getElementById(id_aba).style.display = "none";
     }
-
     if(btn == "1"){
-        document.getElementById("btn1").style.backgroundColor = "red";
+        document.getElementById("btn1").style.backgroundColor = "pink";
         document.getElementById("btn1").disabled = true;
         document.getElementById("ab1").style.display = "block";
     }
-
     if(btn == "2"){
-        document.getElementById("btn2").style.background = "red";
+        document.getElementById("btn2").style.backgroundColor = "pink";
         document.getElementById("btn2").disabled = true;
         document.getElementById("ab2").style.display = "block";
     }
-
     if(btn == "3"){
-        document.getElementById("btn3").style.background = "red";
+        document.getElementById("btn3").style.backgroundColor = "pink";
         document.getElementById("btn3").disabled = true;
         document.getElementById("ab3").style.display = "block";
     }
-
     if(btn == "4"){
-        document.getElementById("btn4").style.background = "red";
+        document.getElementById("btn4").style.backgroundColor = "pink";
         document.getElementById("btn4").disabled = true;
         document.getElementById("ab4").style.display = "block";
     }
+}
+
+let contador = null;
+let inicio = 0;
+let fim = 0;
+ function contagem(){
+     if(inicio == fim + 1)contador.claerInverval();
+     else{
+          document.getElementById("valor").innerHTML = inicio;
+          inicio++;
+     }
+ }
+ 
+function iniciar (){
+     inicio = document.getElementById("inicio").value;
+     fim = document.getElementById("fim").value;
+     contador = setInterval (() => {
+          contagem();
+     },1000);
 }
